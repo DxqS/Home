fe453ee25c024ddd6aac3a00a48f4c9a4f549d8d
Dxq <353622088@qq.com>
2017-05-10 21:20:54 +0800

    poster测试

--- PHandle/tt.py
+++ PHandle/tt.py
@@ -14,11 +14,22 @@ poster.save()
 from PIL import Image, ImageDraw, ImageFont
 
 im = Image.open('poster_back.png')
+
+image1 = Image.open('mat1.jpg').convert("RGBA")
+image2 = Image.open('2.jpg').convert("RGBA")
+image3 = Image.open('mat1.jpg').convert("L")
+print image1
+print image2
+# image3.show()
+# print Image.blend(image1, image2, .3).show()
+print Image.composite(image1, image2, image3).show()
+im.show()
 font = 'app.ttf'
 size = 50
 text = u'a大法官ffff'
 ft = ImageFont.truetype(font, size) if font else None
 ss = ImageDraw.ImageDraw(im)
+print ss.getfont()
 sss = ss.textsize(text, font=ft)[0]
 print sss
 
@@ -31,7 +42,7 @@ ft = ImageFont.truetype(font, size)
 # len_o = len(user_name) - len_c
 #
 # user_name_x = int(375 - float(size * len_c + size * len_o / 2) / 2)
-draw.text([320-sss/2, 198], text, font=ft)
+draw.text([320 - sss / 2, 198], text, font=ft)
 text2 = u'$$我爱你%\nv环环a'
-draw.multiline_text([320-sss/2, 250], text2, font=ft,align="center")
+draw.multiline_text([320 - sss / 2, 250], text2, font=ft, align="center")
 im.save('1.jpg')
